5b10fd90555a0abee7bdef646ee09ff3c117cb74
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-16 12:07:17 +0500

    add retrofit library and fetch json

--- app/src/main/java/com/bakigoal/mars/network/MarsApiService.kt
+++ app/src/main/java/com/bakigoal/mars/network/MarsApiService.kt
@@ -1,3 +1,24 @@
 package com.bakigoal.mars.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 private const val BASE_URL = "https://mars.udacity.com/"
+
+private val retrofit = Retrofit.Builder()
+    .baseUrl(BASE_URL)
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .build()
+
+interface MarsApiService {
+
+    @GET("realestate")
+    fun getProperties(@Query("filter") type: String = ""): Call<String>
+}
+
+object MarsApi {
+    val retrofitService: MarsApiService by lazy { retrofit.create(MarsApiService::class.java) }
+}
--- app/src/main/java/com/bakigoal/mars/overview/OverviewViewModel.kt
+++ app/src/main/java/com/bakigoal/mars/overview/OverviewViewModel.kt
@@ -3,27 +3,32 @@ package com.bakigoal.mars.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.bakigoal.mars.network.MarsApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData String that stores the status of the most recent request
     private val _response = MutableLiveData<String>()
 
-    // The external immutable LiveData for the request status String
     val response: LiveData<String>
         get() = _response
 
-    /**
-     * Call getMarsRealEstateProperties() on init so we can display status immediately.
-     */
     init {
         getMarsRealEstateProperties()
     }
 
-    /**
-     * Sets the value of the status LiveData to the Mars API status.
-     */
     private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
+        MarsApi.retrofitService.getProperties().enqueue(object : Callback<String> {
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = t.message
+            }
+        })
     }
 }
